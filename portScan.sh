639ef17b897290fbf3268f8567d6089eecc23907
d4sh&r <d4sh3r@openmailbox.org>
2016-11-08 15:19:15 -0600

    Update portScan.sh

--- portScan.sh
+++ portScan.sh
@@ -2,7 +2,7 @@
 IP=$1
 function scan
 {
-for ((port=1; port<=65535; port++))
+for ((port=445; port<=445; port++))
 do 
 echo -ne "$port\r";sleep .1s
 (echo >/dev/tcp/$IP/$port) > /dev/null 2>&1 && echo "$port open"
